5e400b06878917fb287d8256594bb556126f7946
Nick <fastnick94@gmail.com>
2014-09-11 19:42:29 -0400

    Added weather to the dashboard

--- app/scripts/controllers/application_controller.js
+++ app/scripts/controllers/application_controller.js
@@ -10,5 +10,34 @@ Emberjsdemo.ApplicationController = Ember.Controller.extend({
       Emberjsdemo.set('isTodo', 1);
     }
 
+
+
+    Ember.run.scheduleOnce('afterRender', this, function(){
+
+      var subtractSpace = 240;
+      $('#weather').height($( window ).height() - subtractSpace);
+
+      $( window ).resize(function() {
+        $('#weather').height($( window ).height() - subtractSpace);
+      });
+
+      $.simpleWeather({
+      location: 'North Dartmouth, MA',
+      woeid: '',
+      unit: 'f',
+      success: function(weather) {
+        html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+        html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+        html += '<li class="currently">'+weather.currently+'</li>';
+        html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+
+        $("#weather").html(html);
+      },
+      error: function(error) {
+        $("#weather").html('<p>'+error+'</p>');
+      }
+    });
+    });
+
   }.observes('currentPath')
 });
